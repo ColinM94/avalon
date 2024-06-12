4caede7b4edec85273080cb4de1c160f46ea7659
Colin Maher <colinmaher94@gmail.com>
2024-06-12 12:04:08 +0200

    Add label to Divider

--- src/components/divider/divider.tsx
+++ src/components/divider/divider.tsx
@@ -4,14 +4,17 @@ import styles from "./styles.module.scss";
 import { Props } from "./types";
 
 export const Divider = (props: Props) => {
-  const { direction = "horizontal", className } = props;
+  const { direction = "horizontal", label, className } = props;
 
   return (
     <div
       className={classes(
+        styles.container,
         direction === "horizontal" ? styles.horizontal : styles.vertical,
         className
       )}
-    />
+    >
+      {label && <div className={styles.label}>{label}</div>}
+    </div>
   );
 };
