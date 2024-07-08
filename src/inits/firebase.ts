9551edec7c92587636b4b1a7404acb66a961f460
Colin Maher <colinmaher94@gmail.com>
2024-07-09 00:56:00 +0200

    fix: attempt #3

--- src/inits/firebase.ts
+++ src/inits/firebase.ts
@@ -1,12 +1,10 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // The external firebase configuration.
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
@@ -15,17 +13,15 @@ const firebaseConfig = {
 };
 
 let firebaseApp;
-let auth;
 let storage;
 let db;
 
 try {
   firebaseApp = initializeApp(firebaseConfig);
-  auth = getAuth(firebaseApp);
   storage = getStorage(firebaseApp);
   db = getFirestore(firebaseApp);
 } catch (error) {
   console.error(error);
 }
 
-export { db, auth, storage };
+export { db, storage };
